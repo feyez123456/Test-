39337cef17297b5f40f48ed219a8a45a7db46afa
feyez123456 <42380487+feyez123456@users.noreply.github.com>
2018-08-16 16:42:50 +0200

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,20 @@
+
+function fontfunction(){
+var x=document.getElementById("fonts").value;
+	document.getElementById("para").style.fontFamily=x;}
+function sizee(){
+	var a=document.getElementById("size").value;
+document.getElementById("para").style.fontSize=a;
+ } 
+
+function one(){var f=document.getElementById("functionbold").value;
+document.getElementById("para").style.fontWeight="bold"
+
+
+
+}
+function two(){var k=document.getElementById("functionitalic").value;
+document.getElementById("para").style.fontStyle="italic"; } 
+function three(){var s=document.getElementById("functionunderline").value;
+document.getElementById("para").style.textDecoration="underline"; } 
+
