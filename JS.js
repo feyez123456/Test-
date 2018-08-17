9cebc0d463f28b8916d33e08e6c89a0a2e717ffe
feyez123456 <42380487+feyez123456@users.noreply.github.com>
2018-08-17 15:10:42 +0200

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,20 +1,31 @@
 
-function fontfunction(){
-var x=document.getElementById("fonts").value;
-	document.getElementById("para").style.fontFamily=x;}
-function sizee(){
-	var a=document.getElementById("size").value;
-document.getElementById("para").style.fontSize=a;
- } 
-
-function one(){var f=document.getElementById("functionbold").value;
-document.getElementById("para").style.fontWeight="bold"
 
 
 
-}
-function two(){var k=document.getElementById("functionitalic").value;
-document.getElementById("para").style.fontStyle="italic"; } 
-function three(){var s=document.getElementById("functionunderline").value;
-document.getElementById("para").style.textDecoration="underline"; } 
+  $("#functionbold").click(
+  function (){
 
+$('#para').css("font-weight" , "bold");
+
+}); 
+ $("#functionitalic").click(
+  function (){
+
+$('#para').css("font-style" , "italic");
+
+});  
+ $("#functionunderline").click(
+  function (){
+
+$('#para').css("text-decoration" , "underline");
+
+});  
+ 
+$("#fonts").change (function() {
+    var x = $("#fonts").val();
+    $("#para").css("font-family" , x); 
+    });   
+$("#size").change (function() {
+    var d = $("#size").val();
+    $("#para").css("font-size" , d); 
+    });   
